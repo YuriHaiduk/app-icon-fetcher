df78874c6fc61673585dc666cad0515b2f1e1374
Yuri Haiduk <yurihaiduk@gmail.com>
2026-06-05 21:01:17 +0300

    hotfix

--- backend/modules/AppIconFetcher/Presentation/Http/Controllers/Api/FetchAppIconsController.php
+++ backend/modules/AppIconFetcher/Presentation/Http/Controllers/Api/FetchAppIconsController.php
@@ -6,7 +6,6 @@ namespace Modules\AppIconFetcher\Presentation\Http\Controllers\Api;
 
 use Illuminate\Http\JsonResponse;
 use Modules\AppIconFetcher\Application\Services\FetchAppIconsService;
-use Modules\AppIconFetcher\Infrastructure\Exceptions\InvalidAppInputException;
 use Modules\AppIconFetcher\Presentation\Http\Requests\FetchAppIconsRequest;
 use Modules\AppIconFetcher\Presentation\Http\Resources\FetchAppIconsResource;
 
@@ -16,19 +15,12 @@ final readonly class FetchAppIconsController
         private FetchAppIconsService $fetchAppIcons,
     ) {}
 
-    public function __invoke(FetchAppIconsRequest $request): FetchAppIconsResource|JsonResponse
+    public function __invoke(FetchAppIconsRequest $request): JsonResponse
     {
-        try {
-            return new FetchAppIconsResource(
-                $this->fetchAppIcons->fetch($request->appInput()),
-            );
-        } catch (InvalidAppInputException $exception) {
-            return response()->json([
-                'message' => $exception->getMessage(),
-                'errors' => [
-                    'input' => [$exception->getMessage()],
-                ],
-            ], 422);
-        }
+        return new FetchAppIconsResource(
+            $this->fetchAppIcons->fetch($request->appInput()),
+        )
+            ->response()
+            ->setStatusCode(200);
     }
 }
